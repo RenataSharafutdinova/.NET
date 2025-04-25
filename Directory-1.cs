6af18c21a7045ff8797f6a51bea4c82f77a2f4ff
RenataSharafutdinova <160972116+RenataSharafutdinova@users.noreply.github.com>
2025-04-25 14:23:03 +0300

    Create Directory-1.cs

---
+++ Directory-1.cs
@@ -0,0 +1,23 @@
+string path = @"C:/Example";
+string subfolder1 = @"/Subfolder1";
+string subfolder2 = @"/Subfolder2";
+
+if (!Directory.Exists(path))
+{
+    Directory.CreateDirectory(path);
+}
+Directory.CreateDirectory($"{path}/{subfolder1}");
+Console.WriteLine("Каталог C:/Example/Subfolder1 создан");
+
+Directory.CreateDirectory($"{path}/{subfolder2}");
+Console.WriteLine("Каталог C:/Example/Subfolder2 создан");
+
+if (Directory.Exists(path))
+{
+    Directory.Delete(path, true);
+    Console.WriteLine("Каталог C:Example удален");
+}
+else
+{
+    Console.WriteLine("Каталог не существует");
+}
